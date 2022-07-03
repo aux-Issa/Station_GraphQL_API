84bf1a9d55fda2d051b7ecacd8e069d81defa617
aux-Issa <issa1116184028@gmail.com>
2022-07-04 00:05:01 +0900

    edited resolver

---
+++ app/graph/schema.resolvers.go
@@ -0,0 +1,43 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aux-Issa/Station_GraphQL_API/graph/generated"
+	"github.com/aux-Issa/Station_GraphQL_API/graph/model"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) StationByName(ctx context.Context, stationName *string) ([]*model.Station, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) StationByCd(ctx context.Context, stationCd *int) (*model.Station, error) {
+	return r.getStationByCD(ctx, stationCd)
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}

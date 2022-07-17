06f372b2f484c8289275751a0d938b7fcd420b85
aux-Issa <issa1116184028@gmail.com>
2022-07-17 23:42:26 +0900

    config routing and resolve CORS

--- api/server.go
+++ api/server.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/aux-Issa/Station_GraphQL_API/graph"
 	"github.com/aux-Issa/Station_GraphQL_API/graph/generated"
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
+	"log"
+	"net/http"
+	"os"
 )
 
 const defaultPort = "8080"
@@ -18,12 +21,42 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
+	//
+	router := chi.NewRouter()
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:8080", "http://localhost:3000"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{
+			"Content-Type",
+		},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// Check against your desired domains here: '*'allows all origins
+				return r.Host == "*"
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
 
+	// routing
+	// rootでgraphql play ground
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	// /graphql をフロントエンドからのリクエストをうけつける
+	router.Handle("/graphql", srv)
+	// todo:port番号8080を環境変数にする
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		panic(err)
+	}
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
